5d4cbec779333986777bff690b3e5bef0e152d42
Eric Nascimento <oiericoi@hotmail.com>
2024-11-03 16:07:14 -0300

    refactor: modify socket config

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,39 +1,25 @@
 import { io, Socket } from "socket.io-client";
 
 const URL = "http://localhost:3333";
-let socket: Socket; // Permitir que socket seja null
+let socket: Socket | null = null;
 
-export const connectSocket =  (): Socket => {
+export const connectSocket = (): Socket => {
     if (!socket) {
         socket = io(URL, {
             transports: ["websocket"],
             autoConnect: true,
             reconnection: true,
-            reconnectionAttempts: Infinity, // Tenta reconectar indefinidamente
-            reconnectionDelay: 1000, // Espera 1 segundo entre tentativas
-            reconnectionDelayMax: 5000, // Tempo máximo de espera de 5 segundos
-            timeout: 20000 // Tempo limite de 20 segundos para conexão
+            reconnectionAttempts: Infinity,
+            reconnectionDelay: 1000,
+            reconnectionDelayMax: 5000,
+            timeout: 20000,
         });
 
-        socket.on("connect", () => {
-            console.log("Conectado ao servidor Socket.IO:", socket?.id);
-        });
-
-        socket.on("disconnect", () => {
-            console.log("Desconectado do servidor Socket.IO");
-        });
-
-        socket.on("reconnect_attempt", (attempt) => {
-            console.log(`Tentando reconectar, tentativa ${attempt}`);
-        });
-
-        socket.on("reconnect", (attempt) => {
-            console.log(`Reconectado ao servidor na tentativa ${attempt}`);
-        });
-
-        socket.on("reconnect_failed", () => {
-            console.log("Falha na reconexão");
-        });
+        socket.on("connect", () => console.log("Conectado ao servidor Socket.IO:", socket?.id));
+        socket.on("disconnect", () => console.log("Desconectado do servidor Socket.IO"));
+        socket.on("reconnect_attempt", (attempt) => console.log(`Tentando reconectar, tentativa ${attempt}`));
+        socket.on("reconnect", (attempt) => console.log(`Reconectado ao servidor na tentativa ${attempt}`));
+        socket.on("reconnect_failed", () => console.log("Falha na reconexão"));
     }
-    return socket; // Retorna o socket, que pode ser null
+    return socket;
 };
