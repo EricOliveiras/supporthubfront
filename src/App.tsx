bfb4eccf12f0787610b725241306934d8e73c701
Eric Nascimento <oiericoi@hotmail.com>
2024-11-03 16:14:07 -0300

    refactor: modify App file

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,42 @@ import {Dashboard} from "@/pages/Dashboard.tsx";
 import {PrivateRoute} from "@/components/private-route.tsx";
 import {UserManagement} from "@/pages/UserManagement.tsx";
 import {SettingsPage} from "@/pages/UserSettings.tsx";
+import {Toaster} from "@/components/ui/toaster.tsx";
+import {SocketProvider} from "@/config/SocketContext.tsx";
+import {StatisticsPage} from "@/pages/Stats.tsx";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Navigate to="/login"/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/dashboard" element={
-                    <PrivateRoute>
-                        <Dashboard />
-                    </PrivateRoute>
-                }/>
-                <Route path="/painel/usuarios" element={
-                    <PrivateRoute>
-                        <UserManagement />
-                    </PrivateRoute>
-                }/>
-                <Route path="/configuracoes" element={
-                    <PrivateRoute>
-                        <SettingsPage />
-                    </PrivateRoute>
-                }/>
-            </Routes>
-        </Router>
+        <SocketProvider>
+            <Toaster/>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/login"/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/dashboard" element={
+                        <PrivateRoute>
+                            <Dashboard/>
+                        </PrivateRoute>
+                    }/>
+                    <Route path="/painel/usuarios" element={
+                        <PrivateRoute>
+                            <UserManagement/>
+                        </PrivateRoute>
+                    }/>
+                    <Route path="/configuracoes" element={
+                        <PrivateRoute>
+                            <SettingsPage/>
+                        </PrivateRoute>
+                    }/>
+                    <Route path="/stats" element={
+                        <PrivateRoute>
+                            <StatisticsPage/>
+                        </PrivateRoute>
+                    }/>
+                </Routes>
+            </Router>
+        </SocketProvider>
+
     );
 }
 
